79af2ba2ca68a7643a54de94616b5587c0f43c40
Julio Felix <julio.felix@deway.com.br>
2021-09-13 19:07:15 -0300

    chore: add Admin Models

--- establishment/admin.py
+++ establishment/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
+admin.site.register(Address)
+admin.site.register(Establishment)
+admin.site.register(Schedules)
+admin.site.register(Ticket)
+admin.site.register(Comments)
 # Register your models here.
--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from events.models import Events, Banner
+
+admin.site.register(Events)
+admin.site.register(Banner)
